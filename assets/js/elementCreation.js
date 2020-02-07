303c569e0f42f37f936f3c9768317e0dc28a38b7
aymen <aymen.sellaouti@gmail.com>
2020-02-07 09:18:47 +0100

    second commit

--- assets/js/elementCreation.js
+++ assets/js/elementCreation.js
@@ -1,1 +1,12 @@
 let img = document.createElement('img');
+
+img.src = 'assets/images/job.png';
+img.title = 'job offer';
+img.alt = 'job offer';
+
+let ol1 =  document.querySelector('#listeVilles');
+
+setInterval(
+    () => ol1.appendChild(img), 2000
+)
+
